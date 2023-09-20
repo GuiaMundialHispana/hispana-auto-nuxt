b2a8ba30a1da75d2335e9acaed51bef0165bf252
DulcePC <d.perez@kation.com.do>
2023-09-20 18:48:14 -0400

    :sparkles: add featured ads page

--- components/organism/Header.vue
+++ components/organism/Header.vue
@@ -124,10 +124,10 @@ export default {
         name: 'Comprar',
         route: '/search?condition=New&priceType=RD',
       },
-      // {
-      //   name: 'Destacados',
-      //   route: '/featured'
-      // },
+      {
+        name: 'Destacados',
+        route: '/featuredProperties'
+      },
       {
         name: 'Contacto',
         route: '/contact'
--- pages/FeaturedProperties.vue
+++ pages/FeaturedProperties.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="w-full md:h-[450px] lg:h-[500px] relative pt-6 sm:pt-16 md:pt-20 mx-auto  border-b border-gray-300 flex items-center justify-start ">
+  <section class="w-full md:h-[450px] lg:h-[500px] relative py-6 sm:py-16 md:py-20 mx-auto  border-b border-gray-300 flex items-center justify-start">
     <div class="md:pl-14 relative z-10 md:text-left text-center px-4 max-w-[97rem] w-full mx-auto text-neutral-white">
       <h1 class="lg:whitespace-nowrap text-neutral-white">Estás más cerca del <br class="hidden sm:block"/><span class="font-bold">hogar de tus sueños</span>
       </h1>
@@ -17,6 +17,6 @@
     </p>
   </section>
   <OrganismExclusiveVehicles />
-  <OrganismVipVehicles  />
+  <OrganismVipVehicles class="mb-20"  />
   <OrganismSilverVehicles class="mb-20" />
 </template>

b8e69fc2391cf8cc2a305d858dae9d069c365ac5
DulcePC <d.perez@kation.com.do>
2023-11-16 17:42:24 -0400

    arreglando section de explore properties

--- components/organism/ExclusiveVehicles.vue
+++ components/organism/ExclusiveVehicles.vue
@@ -29,33 +29,33 @@
         <MoleculesFeaturedVehicles :property-id="plan.id" plantype="exclusive" :property="plan.auto" />
       </swiper-slide>
     </Swiper>
-    <Swiper
-      v-if="pending"
-      :modules="[SwiperFreeMode, SwiperNavigation, SwiperAutoplay]"
-      :effect="'fade'"
-      :lazy="true"
-      :space-between="32"
-      slides-per-view="auto"
-      :autoplay="{
-        delay: 4000,
-        disableOnInteraction: true
-      }"
-    >
-      <swiper-slide v-for="index in 5" :key="index">
-        <div class="skeleton">
-          <div class="skeleton-image"></div>
-          <div class="skeleton-date"></div>
-          <div class="skeleton-body"></div>
-        </div>
-      </swiper-slide>
-    </Swiper>
   </section>
+  <Swiper
+    v-if="pending"
+    :modules="[SwiperFreeMode, SwiperNavigation, SwiperAutoplay]"
+    :effect="'fade'"
+    :lazy="true"
+    :space-between="32"
+    slides-per-view="auto"
+    :autoplay="{
+      delay: 4000,
+      disableOnInteraction: true
+    }"
+  >
+    <swiper-slide v-for="index in 5" :key="index">
+      <div class="skeleton">
+        <div class="skeleton-image"></div>
+        <div class="skeleton-date"></div>
+        <div class="skeleton-body"></div>
+      </div>
+    </swiper-slide>
+  </Swiper>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
 
-const { data: property, pending, error} = await useFetch('advertisements/home?plan=2', {
+const { data: property, pending, error} = await useFetch('advertisements/related/${props.property_id}', {
   method: 'GET',
   baseURL: config.public.API,
   transform:(_property) => _property.results.data

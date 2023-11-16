62cc858e457a724a53657497c577ca507811ac92
DulcePC <d.perez@kation.com.do>
2023-11-16 18:12:09 -0400

    arreglando huevo

--- components/organism/ExclusiveVehicles.vue
+++ components/organism/ExclusiveVehicles.vue
@@ -29,34 +29,33 @@
         <MoleculesFeaturedVehicles :property-id="plan.id" plantype="exclusive" :property="plan.auto" />
       </swiper-slide>
     </Swiper>
+    <Swiper
+      v-if="pending"
+      :modules="[SwiperFreeMode, SwiperNavigation, SwiperAutoplay]"
+      :effect="'fade'"
+      :lazy="true"
+      :space-between="32"
+      slides-per-view="auto"
+      :autoplay="{
+        delay: 4000,
+        disableOnInteraction: true
+      }"
+    >
+      <swiper-slide v-for="index in 5" :key="index">
+        <div class="skeleton">
+          <div class="skeleton-image"></div>
+          <div class="skeleton-date"></div>
+          <div class="skeleton-body"></div>
+        </div>
+      </swiper-slide>
+    </Swiper>
   </section>
-  <Swiper
-    v-if="pending"
-    :modules="[SwiperFreeMode, SwiperNavigation, SwiperAutoplay]"
-    :effect="'fade'"
-    :lazy="true"
-    :space-between="32"
-    slides-per-view="auto"
-    :autoplay="{
-      delay: 4000,
-      disableOnInteraction: true
-    }"
-  >
-    <swiper-slide v-for="index in 5" :key="index">
-      <div class="skeleton">
-        <div class="skeleton-image"></div>
-        <div class="skeleton-date"></div>
-        <div class="skeleton-body"></div>
-      </div>
-    </swiper-slide>
-  </Swiper>
 </template>
 
 <script setup>
-const props = defineProps(['property_id'])
 const config = useRuntimeConfig();
 
-const { data: property, pending, error} = await useFetch(`advertisements/related/${props.property_id}`, {
+const { data: property, pending, error} = await useFetch('advertisements/home?plan=2', {
   method: 'GET',
   baseURL: config.public.API,
   transform:(_property) => _property.results.data

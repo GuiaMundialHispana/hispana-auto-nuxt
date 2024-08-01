f41d12331eccdf71a42a2521d3aa655449647b14
Dulce Perez <d.perez@kation.com.do>
2024-08-01 11:24:00 -0400

    :bug: test slides vercel

--- components/population/SearchDetail/VehicleSlides.vue
+++ components/population/SearchDetail/VehicleSlides.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-    <swiper
+  <section class="swiper-hero-property">
+    <Swiper
       :style="{
         '--swiper-navigation-color': '#fff',
         '--swiper-pagination-color': '#fff',
@@ -8,23 +8,30 @@
       :spaceBetween="10"
       :navigation="true"
       :thumbs="{ swiper: thumbsSwiper }"
-      :modules="[SwiperNavigation,SwiperThumbs]"
-      class="swiper-hero">
+      :modules="[SwiperNavigation,SwiperThumbs, SwiperFreeMode]"
+      :effect="'fade'"
+      :slides-per-view="1"
+      class="swiper-hero"
+      >
         <swiper-slide
-          @click="showModal = true"
-          v-for="image in images" :key="image"
+          v-for="image in images"
+          :key="image"
         >
           <div class="overlay">
             <AtomsIcon name="general/zoom" :size=35 class="text-neutral-white" />
           </div>
           <img :src="`${image.image}`" class="w-full h-full object-cover rounded-lg" />
+          <atoms-property-plans :plan-type="renderPlanText" plan-position="top" class="z-20 top-0 absolute !rounded-none" />
         </swiper-slide>
-        <atoms-property-plans :plan-type="renderPlanText" plan-position="top" />
-    </swiper>
+    </Swiper>
     <!--  -->
+
     <!-- Thumbs -->
     <swiper
       @swiper="setThumbsSwiper"
+      :freeMode="true"
+      :modules="[SwiperThumbs, SwiperFreeMode]"
+      watch-slides-progress
       :spaceBetween="10"
       class="swiper-thumbs"
       :breakpoints="{
@@ -36,6 +43,7 @@
         <img :src="`${image.image}`" class="w-full h-full object-cover rounded-lg" />
       </swiper-slide>
     </swiper>
+
     <!-- Zoom image -->
     <PopulationSearchDetailModalZoomSlides
       :modalImages="images"
@@ -46,7 +54,7 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 
 const props = defineProps(['images','planType']);
 
@@ -64,41 +72,7 @@ const renderPlanText = computed(() => {
   } else if ( props.planType === 2) {
     return 'exclusive';
   } else if(props.planType === 4 ) {
-    return 'destacado';
+    return 'featured';
   }
 });
 </script>
-
-<style lang="postcss" scoped>
-
-section {
-  @apply md:px-14 px-4 mb-10 gap-5 mt-8 2xl:max-w-[1440px] mx-auto grid xl:grid-cols-[auto,205px];
-}
-
-.swiper-hero {
-  @apply md:h-[560px] h-[360px] max-w-6xl flex-grow m-0 !important;
-
-  & .swiper-wrapper {
-    @apply h-full w-full rounded-lg overflow-hidden m-0;
-  }
-
-  & > span { @apply absolute z-10 rounded-lg top-4 left-4 h-8 !important; }
-
-  & .overlay { @apply bg-neutral-black bg-opacity-50 z-[65] absolute w-full h-full bottom-0 left-0 hidden items-center justify-center p-4 cursor-pointer; }
-
-  & .swiper-slide:hover > .overlay { @apply flex; }
-
-}
-
-.swiper-thumbs {
-  @apply xl:w-52 w-full xl:h-[560px] h-32 ;
-
-  & .swiper-wrapper {
-    @apply w-52 h-full m-0 !important;
-
-    & .swiper-slide { @apply w-52 h-32 xl:mx-0 mr-4 mb-4 object-cover !important; }
-  }
-}
-
-.swiper-slide { @apply rounded-lg overflow-hidden relative !important; }
-</style>

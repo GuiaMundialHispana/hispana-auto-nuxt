8b6cd3b6d9a6da7afea2545b332740a282c25a04
Dulce Perez <d.perez@kation.com.do>
2024-08-02 17:56:23 -0400

    :bug: fix hero and text for feature property

--- pages/FeaturedProperties.vue
+++ pages/FeaturedProperties.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="w-full md:h-[450px] lg:h-[500px] relative py-6 sm:py-16 md:py-20 mx-auto  border-b border-gray-300 flex items-center justify-start">
     <div class="md:pl-14 relative z-10 md:text-left text-center px-4 max-w-[97rem] w-full mx-auto text-neutral-white">
-      <h1 class="lg:whitespace-nowrap text-neutral-white">Estás más cerca del <br class="hidden sm:block"/><span class="font-bold">carro de tus sueños</span>
+      <h1 class="lg:whitespace-nowrap text-[#4a4141] ">Estás más cerca del <br class="hidden sm:block"/><span class="font-bold">carro de tus sueños</span>
       </h1>
-      <p class="pt-6 md:pt-8 text-sm lg:whitespace-nowrap">Cada vehículo tiene una historia<strong>descubre la tuya con nosotros</strong></p>
+      <p class="pt-6 md:pt-8 text-sm lg:whitespace-nowrap text-neutral-white">Cada vehículo tiene una historia<strong>descubre la tuya con nosotros</strong></p>
       <AtomsLink
         linkTo="/search?condition=New&priceType=RD" 
         btn-size="large" 
         class="my-5 font-semibold">
-        Buscar más propiedades
+        Buscar más vehículos
       </AtomsLink>
     </div>
     <img src="/img/hero.png" alt="Building" class="absolute z-0 top-0 w-full h-full object-cover transform -scale-x-100 ">
